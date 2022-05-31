5b3d7bae40cc4d90bd9d2719b31d5335a5e0d97c
isso-719 <i@kazukiisogai.net>
2022-05-31 11:10:40 +0900

    add assert template func

--- HelloWorld/hello_test.go
+++ HelloWorld/hello_test.go
@@ -5,10 +5,26 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello Chris"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCollectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying helloworld to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assertCollectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
