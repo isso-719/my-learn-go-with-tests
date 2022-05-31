431c5f03009c0c79f5e76d0d6dc5100d450cd43e
isso-719 <i@kazukiisogai.net>
2022-05-31 11:17:51 +0900

    add 2 lang

--- HelloWorld/hello_test.go
+++ HelloWorld/hello_test.go
@@ -15,16 +15,18 @@ func TestHello(t *testing.T) {
 	t.Run("saying helloworld to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-
 		assertCollectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
+		assertCollectMessage(t, got, want)
+	})
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCollectMessage(t, got, want)
 	})
 }
